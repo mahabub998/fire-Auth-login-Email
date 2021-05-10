f494fd224bf1617364d0fbfc5bcbe4821e13294b
mahabub alam <mahabu998@gmail.com>
2021-05-10 15:11:34 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9EnKqYNjkxB8zI61TSMgdASui3zcCmmQ",
+    authDomain: "fir-recap-3ef86.firebaseapp.com",
+    projectId: "fir-recap-3ef86",
+    storageBucket: "fir-recap-3ef86.appspot.com",
+    messagingSenderId: "749605939691",
+    appId: "1:749605939691:web:d1d1af3906f47d341251ae"
+  };
+  export default firebaseConfig ;
